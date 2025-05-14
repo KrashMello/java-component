0b0034bad5eba7b841c27b472ac38ac507676f82
KrashMello <joelenrikesoteldo@gmail.com>
2025-05-13 22:19:23 -0400

    feat: update container component

--- src/Components/km/Container/Container.java
+++ src/Components/km/Container/Container.java
@@ -1,9 +1,31 @@
 package Components.km.Container;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
 
 public class Container extends JPanel {
   public Container() {
+    setLayout(new BorderLayout());
+    setBackground(Color.DARK_GRAY);
+    // if (args.components != null) {
+    // for (ComponentsType components : args.components) {
+    // if (components.borderLayout == null) {
+    // add(components.component);
+    // } else {
+    // add(components.component, components.borderLayout);
+    // }
+    // }
+    // }
+  }
 
+  public void setBorderTitle(String title) {
+    setBorder(BorderFactory.createTitledBorder(title));
+  }
+
+  public void setBorderless() {
+    setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
   }
 }

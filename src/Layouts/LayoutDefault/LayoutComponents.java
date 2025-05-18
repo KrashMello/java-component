7178842dacdb45b1ce97c4a832895c3012f68888
KrashMello <joelenrikesoteldo@gmail.com>
2025-05-18 15:02:47 -0400

    feat: update layout implement theme colors

--- src/Layouts/LayoutDefault/LayoutComponents.java
+++ src/Layouts/LayoutDefault/LayoutComponents.java
@@ -10,55 +10,70 @@ import javax.swing.JLabel;
 
 import Components.km.Button.Button;
 import Components.km.Container.Container;
+import Components.km.Title.Title;
 import Components.km.Window.ComponentsType;
+import Config.Colors;
 
 class LayoutComponents extends ComponentsType {
+
+  Container containerMain = new Container();
+  Container containerNorth = new Container();
+  Container containerCenter = new Container();
+  Container containerButton = new Container();
+  Container containerCenterInCenter = new Container();
+  Container containerLeftInCenter = new Container();
+
   public LayoutComponents() {
-    this.component = containerMain();
+    this.component = getContainerMain();
   }
 
-  private Container containerMain() {
-    Container container = new Container();
-    container.add(centerContainer(), BorderLayout.CENTER);
-    container.add(containerNorth(), BorderLayout.NORTH);
-    return container;
+  private Container getContainerMain() {
+    containerMain.add(getContainerCenter(), BorderLayout.CENTER);
+    containerMain.add(getContainerNorth(), BorderLayout.NORTH);
+    return containerMain;
   }
 
-  private Container containerNorth() {
-    Container container = new Container();
-    container.setBorderTitle("north container");
-    container.add(buttonContainer(), BorderLayout.EAST);
-    return container;
+  private Container getContainerNorth() {
+    containerNorth.setBorderless();
+    Title label = new Title("label in north container");
+    containerNorth.add(label, BorderLayout.WEST);
+    containerNorth.add(getContainerButton(), BorderLayout.EAST);
+    return containerNorth;
   }
 
-  private Container buttonContainer() {
-    Container container = new Container();
+  private Container getContainerButton() {
+    containerButton.setLayout(new GridLayout(1, 2, 1, 0));
     Button closeButton = new Button("X");
+    Button toggleThemeButton = new Button("Theme");
     closeButton.addActionListener(e -> System.exit(0));
-    container.add(closeButton, BorderLayout.EAST);
-    return container;
+    toggleThemeButton.addActionListener(e -> {
+      Colors.toggleTheme();
+      containerNorth.changeTheme();
+      containerCenter.changeTheme();
+    });
+    toggleThemeButton.setPreferredSize(new Dimension(50, 30));
+    containerButton.add(toggleThemeButton);
+    containerButton.add(closeButton);
+    return containerButton;
   }
 
-  private Container centerContainer() {
-    Container container = new Container();
-    container.setBorder(BorderFactory.createTitledBorder("center Container"));
-    container.add(leftContainer(), BorderLayout.WEST);
-    container.add(centerInCenterContainer(), BorderLayout.CENTER);
-    return container;
+  private Container getContainerCenter() {
+    containerCenter.setBorder(BorderFactory.createTitledBorder("center Container"));
+    containerCenter.add(getContainerLeftInCenter(), BorderLayout.WEST);
+    containerCenter.add(getContainerCenterInCenter(), BorderLayout.CENTER);
+    return containerCenter;
   }
 
-  private Container leftContainer() {
-    Container container = new Container();
-    container.setBorder(BorderFactory.createTitledBorder("left Container"));
-    container.add(new JLabel("label in left container", JLabel.CENTER), BorderLayout.CENTER);
-    return container;
+  private Container getContainerLeftInCenter() {
+    containerLeftInCenter.setBorder(BorderFactory.createTitledBorder("left Container"));
+    containerLeftInCenter.add(new JLabel("label in left container", JLabel.CENTER), BorderLayout.CENTER);
+    return containerLeftInCenter;
   }
 
-  private Container centerInCenterContainer() {
-    Container container = new Container();
-    container.setBorder(BorderFactory.createTitledBorder("center in center Container"));
-    container.add(new JLabel("label in center center container", JLabel.CENTER), BorderLayout.CENTER);
-    return container;
+  private Container getContainerCenterInCenter() {
+    containerCenterInCenter.setBorder(BorderFactory.createTitledBorder("center in center Container"));
+    containerCenterInCenter.add(new JLabel("label in center center container", JLabel.CENTER), BorderLayout.CENTER);
+    return containerCenterInCenter;
   }
 
 }
